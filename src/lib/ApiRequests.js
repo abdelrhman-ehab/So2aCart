b663139c5f3cc1bee8f7afe2985b249d7dce42a9
abdelrhman-ehab <abdelrhmanehab2904@gmail.com>
2026-02-14 04:52:26 +0200

    test production 2

--- src/lib/ApiRequests.js
+++ src/lib/ApiRequests.js
@@ -1,8 +1,6 @@
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ODExYjI4NjM4NDUwZWM5NjhjYzU5YiIsIm5hbWUiOiJBYmRlbHJobWFuIEVoYWIiLCJyb2xlIjoidXNlciIsImlhdCI6MTc3MDE0MzQ5NiwiZXhwIjoxNzc3OTE5NDk2fQ.LUyeBibyxK9409Ht9Wb7J3qfBpG3yI4jJp_rDXlVWtE'
-
 // get cart data
 export const getCartAPi = async () => {
-    const res = await fetch('http://localhost:3000/api/get-cart')
+    const res = await fetch(`/api/get-cart`)
     const response = await res.json()
     if (!res.ok || response.statusMsg === 'error') {
         throw new Error(response.message || 'Failed Get Cart Data')
@@ -11,9 +9,13 @@ export const getCartAPi = async () => {
     return response
 }
 
+
+console.log(process.env.NEXTAUTH_URL);
+
+
 // get wishlist
 export const getWishlistApi = async () => {
-    const res = await fetch('http://localhost:3000/api/get-wishlist')
+    const res = await fetch(`/api/get-wishlist`)
     const response = await res.json()
     if (!res.ok || response.statusMsg === 'error') {
         throw new Error(response.message || 'Failed Get Cart Data')
